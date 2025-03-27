21dfe272bea36d145c2a3234529b9c6559263103
PKmania <piyushgumbal41@gmail.com>
2025-03-27 23:27:04 +0530

    Move 'RemoteFeedItem' and 'LocalFeedItem' to its specific file.

---
+++ EssentialFeed/Feed Api/RemoteFeedItem.swift
@@ -0,0 +1,12 @@
+//
+//  Created by P K Gumbal on 27/03/25.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
