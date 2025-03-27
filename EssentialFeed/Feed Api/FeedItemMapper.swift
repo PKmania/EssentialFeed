4fa462c141bf71302efd25ff0746714efbf97497
PKmania <piyushgumbal41@gmail.com>
2025-03-27 23:22:42 +0530

    Add 'RemoteFeedItem' data transfer repesentation to decouple the items mapper from 'FeedItem' data models.

--- EssentialFeed/Feed Api/FeedItemMapper.swift
+++ EssentialFeed/Feed Api/FeedItemMapper.swift
@@ -3,31 +3,25 @@
 //
 
 import Foundation
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
 
 internal final class FeedItemMapper {
-  private struct Root: Decodable {
-    let items: [item]
-    var feed: [FeedItem] {
-      return items.map{$0.item}
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
     }
-  }
-
-  private struct item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    var item: FeedItem {
-      return FeedItem(id: id, description: description, location: location, imageURL: image)
+    
+    private static var OK_200:Int {return 200}
+    
+    internal static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return root.items
     }
-  }
-  private static var OK_200:Int {return 200}
-  
-  internal static func map(_ data: Data, response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-    guard response.statusCode == OK_200,
-          let root = try? JSONDecoder().decode(Root.self, from: data) else {
-      return .failure(RemoteFeedLoader.Error.invalidData)
-    }
-    return .success(root.feed)
-  }
 }
--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -5,32 +5,44 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-  private let client: HTTPClient
-  private let url: URL
-  public enum Error : Swift.Error {
-    case connectivity
-    case invalidData
-  }
-  public typealias Result = LoadFeedResult
-
-  
-  public init(client: HTTPClient, url: URL) {
-    self.client = client
-    self.url = url
-  }
-  
-  public func load(completion: @escaping (Result) -> Void) {
-    client.get(from: url, completion: {[weak self] (result) in
-      guard self != nil else {return}
-      switch result {
-      case let .success(data,response):
-        completion(FeedItemMapper.map(data, response: response))
-      case .failure:
-        completion(.failure(Error.connectivity))
-      }
-    })
-  }
-
-  
+    private let client: HTTPClient
+    private let url: URL
+    public enum Error : Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    public typealias Result = LoadFeedResult
+    
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url, completion: {[weak self] (result) in
+            guard self != nil else {return}
+            switch result {
+            case let .success(data,response):
+                completion(RemoteFeedLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        })
+    }
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemMapper.map(data, response: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
 }
 
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem]{
+        return map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
+        }
+    }
+}
