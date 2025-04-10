1004cf97edf040ec56aadccb1c675c531f12fd33
PKmania <piyushgumbal41@gmail.com>
2025-04-10 20:42:07 +0530

    Extract duplicate test helpers into a shared scope.

---
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,13 @@
+//
+//  Created by P K Gumbal on 10/04/25.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
