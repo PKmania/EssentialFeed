737e91a8e0ca882b5c9d1b368f5951b5dedb294b
PKmania <piyushgumbal41@gmail.com>
2025-04-17 22:31:53 +0530

    'LocalFeedLoader' in integration with the 'CoreDataFeedStore' overrides items saved by separate instances, proving we correctly manage the data models on disk.

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -22,19 +22,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, [], "Expected empty feed")
-                
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toLoad: [])
     }
     
     func test_load_deliversItemsSavedOnASeparateInstance() {
@@ -49,19 +37,32 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [saveExp], timeout: 1.0)
 
-        let loadExp = expectation(description: "Wait for load completion")
-        sutToPerformLoad.load { loadResult in
-            switch loadResult {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, feed)
+        expect(sutToPerformLoad, toLoad: feed)
+    }
+    
+    
+    func test_save_overridesItemsSavedOnASeparateInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
 
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-
-            loadExp.fulfill()
+        let saveExp1 = expectation(description: "Wait for save completion")
+        sutToPerformFirstSave.save(firstFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp1.fulfill()
         }
-        wait(for: [loadExp], timeout: 1.0)
+        wait(for: [saveExp1], timeout: 1.0)
+
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformLastSave.save(latestFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 1.0)
+
+        expect(sutToPerformLoad, toLoad: latestFeed)
     }
     
     //MARK: - Helpers
@@ -75,6 +76,22 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
+    
+    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+            let exp = expectation(description: "Wait for load completion")
+            sut.load { result in
+                switch result {
+                case let .success(loadedFeed):
+                    XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
+
+                case let .failure(error):
+                    XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
+                }
+
+                exp.fulfill()
+            }
+            wait(for: [exp], timeout: 1.0)
+        }
     
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
