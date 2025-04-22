47c109a5b285ea1b0efcf4fdbedc3df401ad6ecd
PKmania <piyushgumbal41@gmail.com>
2025-04-22 11:39:23 +0530

    Remove rudandant internal access control declarations.

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -4,14 +4,14 @@
 
 import Foundation
 
-internal final class FeedCachePolicy {
+final class FeedCachePolicy {
     private init() {}
     private static let calender = Calendar(identifier: .gregorian)
     private static var maxCachedAgeInDays: Int {
         return 7
     }
     
-    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calender.date(byAdding: .day, value: maxCachedAgeInDays, to: timestamp) else {
             return false
         }
--- EssentialFeedTests/Feed Api/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed Api/LoadFeedFromRemoteUseCaseTests.swift
@@ -113,9 +113,7 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
       "description": item.description,
       "location": item.location,
       "image": item.url.absoluteString
-    ].reduce(into: [String: Any]()) { (acc, e) in
-      if let value = e.value { acc[e.key] = value }
-    }
+    ].compactMapValues { $0 }
     return (item,json)
   }
   private func makeItemJSON(_ items: [[String: Any]]) -> Data {
