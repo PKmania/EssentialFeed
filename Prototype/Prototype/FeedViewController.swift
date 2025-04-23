4458225c6a08fdbbde734a6bdc48afd5619943f6
Piyush <piyush@capitalnumbers.com>
2025-04-23 17:35:48 +0530

    Dsplay 'FeedImageCell' on tableview list

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by CN23 on 23/04/25.
+//
+
+import UIKit
+
+final class FeedViewController: UITableViewController {
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+  }
+}
