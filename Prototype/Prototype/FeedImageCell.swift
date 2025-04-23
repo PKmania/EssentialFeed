517e06dabf45210bd6ccf9d206021102a1a3eda6
Piyush <piyush@capitalnumbers.com>
2025-04-23 18:01:33 +0530

    Add 'FeedImageCell' with storybaord connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by CN23 on 23/04/25.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+
+  @IBOutlet weak var locationContainer: UIStackView!
+  
+  @IBOutlet weak var feedImage: UIImageView!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+}
