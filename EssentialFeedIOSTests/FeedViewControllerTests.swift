8381ce4842f71dee1f42dbe361d9369b68d44344
Piyush <piyush@capitalnumbers.com>
2025-04-25 07:37:40 +0530

    Replace 'FeedViewControllerTests.LoaderSpy' references in the production types with the production 'FeedLoader' protocol abbstration.

--- EssentialFeedIOSTests/FeedViewControllerTests.swift
+++ EssentialFeedIOSTests/FeedViewControllerTests.swift
@@ -4,15 +4,16 @@
 
 import XCTest
 import UIKit
+import EssentialFeed
 final class FeedViewController: UIViewController {
-  private var loader: FeedViewControllerTests.LoaderSpy?
-  convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+  private var loader: FeedLoader?
+  convenience init(loader: FeedLoader) {
     self.init()
     self.loader = loader
   }
   override func viewDidLoad() {
     super.viewDidLoad()
-    loader?.load()
+    loader?.load { _ in }
   }
 }
 final class FeedViewControllerTests: XCTestCase {
@@ -32,9 +33,10 @@ final class FeedViewControllerTests: XCTestCase {
   }
   
   //MARK: - Helpers
-  class LoaderSpy {
+  class LoaderSpy: FeedLoader {
     private(set) var loadLabelCount:Int = 0
-    func load() {
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
       loadLabelCount += 1
     }
   }
