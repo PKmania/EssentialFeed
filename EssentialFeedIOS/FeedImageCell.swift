31e0863d30c38bf43332bb734953f715d2854e25
Piyush <piyush@capitalnumbers.com>
2025-05-01 16:04:41 +0530

    Feed image view loading indicator is visible while loading image

--- EssentialFeedIOS/FeedImageCell.swift
+++ EssentialFeedIOS/FeedImageCell.swift
@@ -8,7 +8,8 @@ public final class FeedImageCell: UITableViewCell {
   public let locationContainer = UIView()
   public let locationLabel = UILabel()
   public let descriptionLabel = UILabel()
-  
+  public let feedImageContainer = UIView()
+
 
 
 }
