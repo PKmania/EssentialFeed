0478f30a61312478b5c5a975caab51bf99971ce2
Piyush <piyush@capitalnumbers.com>
2025-05-11 00:30:32 +0530

    Move ViewModels to separate files

---
+++ EssentialFeedIOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  Created by CN23 on 11/05/25.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+  let isLoading: Bool
+}
---
+++ EssentialFeedIOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  Created by CN23 on 11/05/25.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+  let feed: [FeedImage]
+}
