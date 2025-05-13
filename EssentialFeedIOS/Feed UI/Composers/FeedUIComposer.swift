11c9c113873165cc2166769551353260da4494e6
Piyush <piyush@capitalnumbers.com>
2025-05-13 17:02:12 +0530

    Move WeakRefVirtualProxy to separate file

--- EssentialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeedIOS/Feed UI/Composers/FeedUIComposer.swift
@@ -32,20 +32,6 @@ private extension FeedViewController {
   }
 }
 
-private final class WeakRefVirtualProxy<T: AnyObject> {
-  private weak var object: T?
-  
-  init(_ object: T) {
-    self.object = object
-  }
-}
-
-extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-  func display(_ viewModel: FeedLoadingViewModel) {
-    object?.display(viewModel)
-  }
-}
-
 private final class FeedViewAdapter: FeedView {
   
   
@@ -91,13 +77,6 @@ private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate{
         self?.presenter?.didFinishLoadingFeed(with: error)
       }
     }
-  }
-}
-
-
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-  func display(_ model: FeedImageViewModel<UIImage>) {
-    object?.display(model)
   }
 }
 
