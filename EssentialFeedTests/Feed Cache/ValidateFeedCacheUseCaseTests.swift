ca26744c21a43bc267c3dcbd745353d3716369af
Piyush <piyush@capitalnumbers.com>
2025-06-08 20:21:44 +0530

    'LocalFeedLoader.validateCache' fails on deletion error of a expired cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -105,6 +105,32 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         store.completeRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
       })
     }
+  
+  func test_validateCache_failsOnDeletionErrorOfExpiredCache() {
+    let feed = uniqueImageFeed()
+    let fixedCurrentDate = Date()
+    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
+    let (store, sut) = makeSUT(currentDate: { fixedCurrentDate })
+    let deletionError = anyNSError()
+
+    expect(sut, toCompleteWith: .failure(deletionError), when: {
+      store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
+      store.completeDeletion(with: deletionError)
+    })
+  }
+
+  func test_validateCache_succeedsOnSuccessfulDeletionOfExpiredCache() {
+    let feed = uniqueImageFeed()
+    let fixedCurrentDate = Date()
+    let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
+    let (store, sut) = makeSUT(currentDate: { fixedCurrentDate })
+
+    expect(sut, toCompleteWith: .success(()), when: {
+      store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
+      store.completeDeletionSuccessfully()
+    })
+  }
+  
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,file: StaticString = #filePath, line: UInt = #line) -> (store: FeedStoreSpy, sut: LocalFeedLoader) {
