15f2e0ebb37dc0b36c6899d8f8c1f3e58be9c82a
Piyush <piyush@capitalnumbers.com>
2025-06-08 13:19:53 +0530

    Make 'RemoteFeedImageDataLoader' conform to 'FeedImageDataLoader'

--- EssentialFeed/Feed Api/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedImageDataLoader.swift
@@ -4,46 +4,47 @@
 
 import Foundation
 
-public final class RemoteFeedImageDataLoader {
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
+  
   private let client: HTTPClient
-
+  
   public init(client: HTTPClient) {
     self.client = client
   }
-
+  
   public enum Error: Swift.Error {
     case invalidData
   }
-
+  
   private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
     private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
+    
     var wrapped: HTTPClientTask?
-
+    
     init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
       self.completion = completion
     }
-
+    
     func complete(with result: FeedImageDataLoader.Result) {
       completion?(result)
     }
-
+    
     func cancel() {
       preventFurtherCompletions()
       wrapped?.cancel()
     }
-
+    
     private func preventFurtherCompletions() {
       completion = nil
     }
   }
-
+  
   @discardableResult
   public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
     let task = HTTPClientTaskWrapper(completion)
     task.wrapped = client.get(from: url) { [weak self] result in
       guard self != nil else { return }
-
+      
       switch result {
       case let .success(data, response):
         if response.statusCode == 200, !data.isEmpty {
