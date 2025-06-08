feec48e293a0a0771cbb1dd177458d746fd93e3a
Piyush <piyush@capitalnumbers.com>
2025-06-08 17:00:44 +0530

    Move 'LocalFeedImageDataLoader' and FeedImageDataStore to production

---
+++ EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -0,0 +1,11 @@
+//
+//  Created by CN23 on 08/06/25.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+  typealias Result = Swift.Result<Data?, Error>
+
+  func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
